85f920597b62b2a401f4cab5e55a074a1e47c1d5
Sam Edelstein <sedelstein@ithaca.edu>
2021-09-28 11:04:54 -0400

    fetch

--- pages/browse.js
+++ pages/browse.js
@@ -1,7 +1,7 @@
 import Head from "next/head";
 import Header from "../components/Navigation/Header";
 
-export default function Browse() {
+export default function Browse(data) {
     return (
         <div>
             <Header title={"Browse Artists"}>
@@ -13,3 +13,19 @@ export default function Browse() {
             <h1>Artists and Genres go here</h1>
         </div>
     )}
+
+export async function getStaticProps(context) {
+    const res = await fetch(`http://localhost:5000/`)
+    const data = await res.json()
+    console.log(data)
+
+    if (!data) {
+        return {
+            notFound: true,
+        }
+    }
+
+    return {
+        props: { data }, // will be passed to the page component as props
+    }
+}
