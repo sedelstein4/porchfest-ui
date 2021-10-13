9d3f9e3a64a16c791ec05852d88069ae5e6074d0
Sam Edelstein <sedelstein@ithaca.edu>
2021-10-13 09:54:20 -0400

    Update [slug].js

--- pages/artist/[slug].js
+++ pages/artist/[slug].js
@@ -1,8 +1,15 @@
 import styled from 'styled-components'
+import Header from "../../components/Navigation/Header";
+import Head from "next/head";
 
 const Artist = ({ author, posts }) => {
     return (
         <div className="content">
+            <Head>
+                <title>Artist Name</title>
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <Header title={"Artist Name"}/>
         </div>
     )
 }
