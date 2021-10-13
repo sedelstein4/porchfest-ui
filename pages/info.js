b5daa06290c4492e92949c7ed795db7b81c2db88
Elias <elias@jwplatt.com>
2021-10-13 01:06:00 -0400

    eventinfo

--- pages/info.js
+++ pages/info.js
@@ -1,15 +1,26 @@
 import Head from "next/head";
 import Header from "../components/Navigation/Header";
+import EventInfo from "../components/Browse/EventInfo/EventInfo";
 
 export default function Info(data) {
     console.log(data)
+    let nameFromBackend = "Porchfest 2021"; //temp
+
+    //need to change header or make new component to allow subheader (city name), also repurpose later for artist pages
+        //and also changing the icon
     return (
         <div className="content">
-            <Header title={"Event Information"}/>
+            <Header title={nameFromBackend}/>
             <Head>
-                <title>Porchfest 2021</title>
+                <title>{nameFromBackend}</title>
                 <link rel="icon" href="/favicon.ico" />
             </Head>
-            <h1>Event info goes here</h1>
+        <EventInfo
+            imgPath={"/images/profile.jpeg"}
+            name={nameFromBackend}
+            dateStr={"September 22nd, 2021"}
+            timeStr={"Noon-6pm"}
+            desc={"Porchfest is a music festival held on the porches of the Fall Creek and Northside neighborhoods of Ithaca, NY."}
+        />
         </div>
     )}
