95c251bee8e22455f6b5b991a0c63a11b27f430d
Sam Edelstein <sedelstein@ithaca.edu>
2021-11-03 12:32:28 -0400

    updated artist images

--- pages/artist/[slug].js
+++ pages/artist/[slug].js
@@ -5,12 +5,30 @@ import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import React from "react";
 import {faApple, faFacebookSquare, faInstagramSquare, faSpotify} from "@fortawesome/free-brands-svg-icons";
 
+const ArtistImageDiv = styled.div`
+  display: inline-block;
+  margin-top: 10px;
+  margin-bottom: 20px;
+  width: 100%;
+  height: 20vw;
+  min-height: 300px;
+  position: relative;
+  overflow: hidden;
+  background-image: url(${(props) => props.imageSrc});
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position: top center;
+
+`
+
 const ArtistImage = styled.img`
   display: block;
   margin: 0 auto;
-  padding-top: 40px;
-  padding-bottom: 20px;
-  width: 75%;
+  width: 250px;
+  height: 250px;
+  object-fit: cover;
+  border-radius: 3px;
+  border: 1px solid var(--grey-middark);
 `
 
 const SocialMediaGrid = styled.div`
@@ -42,8 +60,9 @@ const Artist = ({ data }) => {
                 <link rel="icon" href="/favicon.ico" />
             </Head>
             <Header pageType={"artist"} title={data.artist.name}/>
-                <ArtistImage src={data.artist.photo ? data.artist.photo : "/images/profile.jpeg"}
-                             alt={data.artist.name}/>
+            <ArtistImageDiv imageSrc={data.artist.photo ? data.artist.photo : "/images/profile.jpeg"}>
+                {/*<ArtistImage src={data.artist.photo ? data.artist.photo : "/images/profile.jpeg"} alt={data.artist.name}/>*/}
+            </ArtistImageDiv>
             <SocialMediaGrid>
                 <FontAwesomeIcon icon={faFacebookSquare} />
                 <FontAwesomeIcon icon={faInstagramSquare} />
--- pages/browse.js
+++ pages/browse.js
@@ -38,7 +38,6 @@ export default function Browse(data) {
                                                             {artist.name}
                                                         </a>
                                                     </Link>
-
                                                 </Styles.name>
                                             </Styles.cardContainer>
                                         )
