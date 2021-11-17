7eabda1995f001f3917accc2bb9d062b5d83becf
Elias <elias@jwplatt.com>
2021-11-16 23:23:43 -0500

    selectGenres uses backend data

--- pages/selectGenres.js
+++ pages/selectGenres.js
@@ -1,17 +1,41 @@
 import Head from "next/head";
 import React from "react";
-import GenreChecklist from "../components/Onboarding/Interests/GenreChecklist"
+import * as Styles from "../components/Onboarding/Interests/styles"
 
-export default function selectGenres() {
-    return (
-        <div>
-            <Head>
-                <title>Select Artists</title>
-                <link rel="icon" href="/favicon.ico" />
-            </Head>
-            <GenreChecklist/>
-        </div>
-    )
+export default function selectGenres(data) {
+    console.log(data.genreData)
+    if (data.genreData[0].name) {
+        return (
+            <div className={"content"}>
+                <Head>
+                    <title>Select Artists</title>
+                    <link rel="icon" href="/favicon.ico"/>
+                </Head>
+                <Styles.Header>Select Your Music Interests</Styles.Header>
+                <Styles.Checklist>
+                    <form>
+                        {data.genreData.slice(0).map((genre, i) => {
+                            return (
+                                <Styles.selection htmlFor={genre.name}>{genre.name}
+                                    <input type={"checkbox"} id={genre.name}/>
+                                    <Styles.checkmark/>
+                                </Styles.selection>
+                            )
+                        })}
+                    </form>
+                </Styles.Checklist>
+            </div>
+        )
+    }
 }
 
-//TODO add getLayout (onboarding)
+export async function getStaticProps( context ) {
+    const genreRes = await fetch(`http://localhost:5000/genres`);
+    const genreData = await genreRes.json()
+
+    return {
+        props: {
+            genreData
+        }
+    }
+}
