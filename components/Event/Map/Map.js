1291b1e257cf2d362a3f017de7456fd2361587c5
Elias <elias@jwplatt.com>
2022-02-10 23:28:49 -0500

    expanded map blur infrastructure

--- components/Event/Map/Map.js
+++ components/Event/Map/Map.js
@@ -1,6 +1,6 @@
 import * as Styles from "/components/Event/Map/styles"
 import Navigation from "../../Navigation/Navigation";
-
+import {useState} from "react";
 
 import 'leaflet/dist/leaflet.css'
 
@@ -10,46 +10,78 @@ import * as L from 'leaflet';
 import 'leaflet-defaulticon-compatibility';
 import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet'
 
+const blurred ={
+    filter:"blur(10px)",
+    pointerEvents: "none",
+    touchAction: "none",
+    display:"block",
+    height:"100vh", //otherwise filter will move the nav up behind the map
+    //transition: "filter 0.5s linear" //will this work?
+};
+
+const displayed ={
+    display:"block !important",
+}
+
 export default function Map(props){
+    const [state, setState] = useState('initial')
+    let isBlurred;
+    let isDisplayed;
+
+    const proximityEvent = (event) => { //not sure if this will work or how we will detect proximity to a porch, placeholder
+        setState('blur')
+    }
+
+    if (state === 'blur') {
+        isBlurred = blurred;
+        isDisplayed = displayed;
+    }
+
     return (
-        <Styles.container>
-            <MapContainer
-                center={[42.44242, -76.49921]}
-                zoom={13.5}
-                scrollWheelZoom={true}
-                doubleClickZoom={false}
-                tap={false} //when enabled, very difficult to open popup in mobile view on browser. need to test on real mobile device.
-                style={{height: "calc(100vh - 69px)",
-                    width: "100%",
-                }}>
-                <TileLayer
-                    attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-                    url="https://api.mapbox.com/styles/v1/eplattpf/ckwvajgre0dpt14lhyr0vfncu/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiZXBsYXR0cGYiLCJhIjoiY2t3dmFmOWNwMXk3MzJxanZmemUzM2dkaiJ9.AeD5w-nY9Bm7aXhPfdS4iA"
-                />
-                <Marker position={[42.422660, -76.495167]}>
-                    <Popup>
-                        <span>Williams Hall</span>
-                    </Popup>
-                </Marker>
-                <Marker position={[42.446700, -76.498440]}>
-                    <Popup>
-                        <span>210 Utica St - Cielle</span>
-                    </Popup>
-                </Marker>
-                <Marker position={[42.444860, -76.502120]}>
-                    <Popup>
-                        <span>105 2nd Str - The Flywheels</span>
-                    </Popup>
-                </Marker>
-                <Marker position={[42.449340, -76.500430]}>
-                    <Popup>
-                        <span>219 Auburn St - Lloyd's Boys)</span>
-                    </Popup>
-                </Marker>
-
-
-            </MapContainer>
-            <Navigation/>
-        </Styles.container>
+        <div>
+            <Styles.nowViewing style={{isDisplayed}}>
+                Now Viewing: <br /><br />
+                [display artist info, img, links] <br/><br/>
+                Take a few steps back from the stage to return to the map.
+            </Styles.nowViewing>
+            <div style={isBlurred}>
+                <MapContainer
+                    center={[42.44242, -76.49921]}
+                    zoom={13.5}
+                    scrollWheelZoom={true}
+                    doubleClickZoom={false}
+                    tap={false} //when enabled, very difficult to open popup in mobile view on browser. need to test on real mobile device.
+                    style={{height: "calc(100vh - 69px)",
+                        width: "100%",
+                        display:"block",
+                    }}>
+                    <TileLayer
+                        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                        url="https://api.mapbox.com/styles/v1/eplattpf/ckwvajgre0dpt14lhyr0vfncu/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiZXBsYXR0cGYiLCJhIjoiY2t3dmFmOWNwMXk3MzJxanZmemUzM2dkaiJ9.AeD5w-nY9Bm7aXhPfdS4iA"
+                    />
+                    <Marker position={[42.422660, -76.495167]}>
+                        <Popup>
+                            <span>Williams Hall</span>
+                        </Popup>
+                    </Marker>
+                    <Marker position={[42.446700, -76.498440]}>
+                        <Popup>
+                            <span>210 Utica St - Cielle</span>
+                        </Popup>
+                    </Marker>
+                    <Marker position={[42.444860, -76.502120]}>
+                        <Popup>
+                            <span>105 2nd Str - The Flywheels</span>
+                        </Popup>
+                    </Marker>
+                    <Marker position={[42.449340, -76.500430]}>
+                        <Popup>
+                            <span>219 Auburn St - Lloyd's Boys)</span>
+                        </Popup>
+                    </Marker>
+                </MapContainer>
+                <Navigation/>
+            </div>
+        </div>
     )
 }
--- components/Event/Map/styles.js
+++ components/Event/Map/styles.js
@@ -1,6 +1,10 @@
 // import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet'
 import styled from "styled-components";
 
-export const container = styled.div`
-  
+export const nowViewing = styled.div`
+  position: absolute;
+  margin:50px;
+  top: 30%;
+  left: 16%;
+  z-index:100;
 `
