f56d6edda1f3d00d9b32d95e641317125618a8ae
Sean Blackford <57670508+SeanBlackford37@users.noreply.github.com>
2022-04-01 13:08:33 -0400

    added new endpoints

--- Config.js
+++ Config.js
@@ -4,8 +4,8 @@ export const environment = process.env.REACT_APP_ENVIRONMENT
 export let backendEndpoint
 export let frontendEndpoint
 if (environment === 'prod') {
-    backendEndpoint = '54.167.164.20'
-    frontendEndpoint = ''
+    backendEndpoint = 'https://ec2-54-167-164-20.compute-1.amazonaws.com'
+    frontendEndpoint = 'https://d2zdtmtdrz0btq.cloudfront.net'
 } else {
     backendEndpoint = 'http://localhost:5000/'
     frontendEndpoint = 'http://localhost:3000/'
