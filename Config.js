df5f96707d9e48fe73c84aa080d5932246ea0f64
Sean Blackford <57670508+SeanBlackford37@users.noreply.github.com>
2022-04-01 13:35:31 -0400

    added dotenv

--- Config.js
+++ Config.js
@@ -3,8 +3,9 @@ export const environment = process.env.REACT_APP_ENVIRONMENT
 // define endpoints based on environment
 export let backendEndpoint
 export let frontendEndpoint
+
 if (environment === 'prod') {
-    backendEndpoint = 'https://ec2-54-167-164-20.compute-1.amazonaws.com'
+    backendEndpoint = 'https://test.anime.technology/'
     frontendEndpoint = 'https://d2zdtmtdrz0btq.cloudfront.net'
 } else {
     backendEndpoint = 'http://localhost:5000/'
